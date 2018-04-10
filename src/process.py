5d9b3d77d873c59047f4f8fe25abf7a708ec0c13
Valentin NOEL <valent.noel@gmail.com>
2018-04-10 10:18:04 +0200

    Add worker

---
+++ src/process.py
@@ -0,0 +1,92 @@
+
+import os
+import logging
+import configparser
+import subprocess
+
+class Process():
+
+    def __init__(self):
+        self.load_configuration()
+
+    def get_parameter(self, key, default):
+        key = "COMMAND_" + key
+        if key in os.environ:
+            return os.environ.get(key)
+        return default
+
+    def load_configuration(self):
+        self.command_bin_path = self.get_parameter('BIN_PATH', '/usr/bin')
+        self.command_lib_path = self.get_parameter('LIB_PATH', '/usr/lib')
+        self.env = os.environ.copy()
+
+    def launch(self, program: str, inputs: list, outputs: list, lib_path: list):
+
+        if program.startswith("/"):
+            self.command_path = program
+        else:
+            self.command_path = os.path.join(self.command_bin_path, program)
+
+        if "LD_LIBRARY_PATH" not in self.env:
+            self.env["LD_LIBRARY_PATH"] = ""
+
+        for path in lib_path:
+            self.env["LD_LIBRARY_PATH"] += ":" + path
+        self.env["LD_LIBRARY_PATH"] += self.command_lib_path
+
+        command = [self.command_path]
+        dst_paths = []
+
+        for input in inputs:
+            options = input["options"]
+            for key, value in options.items():
+                command.append(key)
+                if value is not True:
+                    command.append(str(value))
+            command.append(input["path"])
+
+        for output in outputs:
+            options = output["options"]
+            for key, value in options.items():
+                command.append(key)
+                if value is not True:
+                    command.append(str(value))
+
+            if "path" in output:
+                dst_path = output["path"]
+                command.append(dst_path)
+
+                # Create missing output directory
+                dst_dir = os.path.dirname(dst_path)
+                if not os.path.exists(dst_dir):
+                    logging.debug("Create output directory: %s", dst_dir)
+                    os.makedirs(dst_dir)
+
+                dst_paths.append(dst_path)
+
+        # Process command
+        logging.debug("Launching process command: %s", ' '.join(command))
+        command_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=self.env)
+        stdout, stderr = command_process.communicate()
+        self.log_subprocess(stdout, stderr)
+
+        if stderr:
+            message = "An error occurred processing "
+            message += inputs + ": "
+            message += stderr.decode("utf-8")
+            raise RuntimeError(message)
+        if command_process.returncode != 0:
+            message = "Process returned with error "
+            message += "(code: " + str(command_process.returncode) + "):\n"
+            message += stdout.decode("utf-8")
+            raise RuntimeError(message)
+
+        return dst_paths
+
+    def log_subprocess(self, stdout, stderr):
+        if stdout:
+            for line in stdout.decode("utf-8").split("\n"):
+                logging.info("[Command Worker] " + line)
+        if stderr:
+            for line in stderr.decode("utf-8").split("\n"):
+                logging.error("[Command Worker] " + line)
---
+++ src/worker.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import configparser
+import json
+import traceback
+import logging
+import os
+
+from amqp_connection import Connection
+from process import Process
+
+conn = Connection()
+
+logging.basicConfig(
+    format="%(asctime)-15s [%(levelname)s] %(message)s",
+    level=logging.DEBUG,
+)
+
+config = configparser.RawConfigParser()
+config.read([
+    'worker.cfg',
+    '/etc/py_command_line_worker/worker.cfg'
+])
+
+def check_requirements(requirements):
+    meet_requirements = True
+    if 'paths' in requirements:
+        required_paths = requirements['paths']
+        assert isinstance(required_paths, list)
+        for path in required_paths:
+            if not os.path.exists(path):
+                logging.debug("Warning: Required file does not exists: %s", path)
+                meet_requirements = False
+
+    return meet_requirements
+
+
+def callback(ch, method, properties, body):
+    try:
+        msg = json.loads(body.decode('utf-8'))
+        logging.debug(msg)
+
+        try:
+            parameters = msg['parameters']
+            if 'requirements' in parameters:
+                if not check_requirements(parameters['requirements']):
+                    return False
+
+            lib_path = []
+            if 'libraries' in parameters:
+                lib_path = parameters["libraries"]
+
+            program = parameters["program"]
+            inputs = parameters["inputs"]
+            outputs = parameters["outputs"]
+
+            process = Process()
+            dst_paths = process.launch(program, inputs, outputs, lib_path)
+
+            logging.info("""End of process from %s to %s""",
+                ', '.join(input["path"] for input in inputs),
+                ', '.join(dst_paths))
+
+            body_message = {
+                "status": "completed",
+                "job_id": msg['job_id'],
+                "output": dst_paths
+            }
+
+            conn.publish_json('job_command_line_completed', body_message)
+
+        except Exception as e:
+            logging.error(e)
+            traceback.print_exc()
+            error_content = {
+                "body": body.decode('utf-8'),
+                "error": str(e),
+                "job_id": msg['job_id'],
+                "type": "job_command_line"
+            }
+            conn.publish_json('job_command_line_error', error_content)
+
+    except Exception as e:
+        logging.error(e)
+        traceback.print_exc()
+        error_content = {
+            "body": body.decode('utf-8'),
+            "error": str(e),
+            "type": "job_command_line"
+        }
+        conn.publish_json('job_command_line_error', error_content)
+    return True
+
+
+conn.run(config['amqp'],
+        'job_command_line',
+        ['job_command_line_completed',
+         'job_command_line_error'],
+        callback
+    )
