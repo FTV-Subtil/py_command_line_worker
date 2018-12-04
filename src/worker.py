cd10fd1b25e37982a92bf55a018364b2e44ffb1c
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-12-04 14:48:38 +0100

    update message structure

--- src/worker.py
+++ src/worker.py
@@ -47,6 +47,48 @@ def check_requirements(requirements):
 
     return meet_requirements
 
+def get_config_parameter(config, key, param):
+    if key in os.environ:
+        return os.environ.get(key)
+
+    if param in config:
+        return config[param]
+    raise RuntimeError("Missing '" + param + "' configuration value.")
+
+def get_parameter(parameters, key, default: None):
+    for parameter in parameters:
+        if parameter['id'] == key:
+            value = None
+            if 'default' in parameter:
+                value = parameter['default']
+
+            if 'value' in parameter:
+                value = parameter['value']
+
+            if(parameter['type'] != 'credential'):
+                return value
+
+            hostname = get_config_parameter(config['backend'], 'BACKEND_HOSTNAME', 'hostname')
+            username = get_config_parameter(config['backend'], 'BACKEND_USERNAME', 'username')
+            password = get_config_parameter(config['backend'], 'BACKEND_PASSWORD', 'password')
+
+            response = requests.post(hostname + '/sessions', json={'session': {'email': username, 'password': password}})
+            if response.status_code != 200:
+                raise("unable to get token to retrieve credential value")
+
+            body = response.json()
+            if not 'access_token' in body:
+                raise("missing access token in response to get credential value")
+
+            headers = {'Authorization': body['access_token']}
+            response = requests.get(hostname + '/credentials/' + value, headers=headers)
+
+            if response.status_code != 200:
+                raise("unable to access to credential named: " + key)
+
+            body = response.json()
+            return body['data']['value']
+    return default
 
 def callback(ch, method, properties, body):
     try:
@@ -56,20 +98,15 @@ def callback(ch, method, properties, body):
         try:
             parameters = msg['parameters']
             if 'requirements' in parameters:
-                if not check_requirements(parameters['requirements']):
+                if not check_requirements(get_parameter(parameters, 'requirements')):
                     return False
 
-            lib_path = []
-            if 'libraries' in parameters:
-                lib_path = parameters["libraries"]
+            lib_path = get_parameter(parameters, 'libraries', [])
+            exec_dir = get_parameter(parameters, 'exec_dir')
 
-            exec_dir = None
-            if 'exec_dir' in parameters:
-                exec_dir = parameters["exec_dir"]
-
-            program = parameters["program"]
-            inputs = parameters["inputs"]
-            outputs = parameters["outputs"]
+            program = get_parameter(parameters, 'program')
+            inputs = get_parameter(parameters, 'inputs')
+            outputs = get_parameter(parameters, 'outputs')
 
             dst_paths = []
             process = Process()
